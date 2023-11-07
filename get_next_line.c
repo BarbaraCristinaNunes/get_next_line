ac760725a3134669966c6039f87d5b22797e2e5a
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2023-11-07 12:32:30 +0100

    add get_next_line_utils.c

--- get_next_line.c
+++ get_next_line.c
@@ -15,77 +15,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-size_t	ft_strlen(const char *s)
-{
-	int	str_length;
-
-	str_length = 0;
-	while (s[str_length] != '\0')
-		str_length++;
-	return (str_length);
-}
-
-int	ft_strfind(const char *s, int c)
-{
-	while (*s != (char)c)
-	{
-		if (*s == '\0')
-			return (0);
-		s++;
-	}
-	return (1);
-}
-
-void	ft_putstr(char *str)
-{
-	write (1, str, ft_strlen(str));
-}
-
-static char	*fill_concat_str(char *concat_str, char const *s1,
-		char const *s2, int concat_str_length)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0' && i <= concat_str_length)
-	{
-		concat_str[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0' && i <= concat_str_length)
-	{
-		concat_str[i] = s2[j];
-		j++;
-		i++;
-	}
-	concat_str[i] = '\0';
-	return (concat_str);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*concat_str;
-	int		length;
-
-	if (s2 == NULL)
-		return ((char *)s1);
-	if (s1 == NULL)
-		return ((char *)s2);
-	length = ft_strlen(s1) + ft_strlen(s2);
-	concat_str = (char *)malloc(length + 1);
-	if (concat_str == NULL)
-		return (0);
-	return (fill_concat_str(concat_str, s1, s2, length));
-}
-
 char	*read_file(int fd, char *buffer)
 {
 	int		bytes_read;
 	char	break_line[1];
+
 	bytes_read = 0;
-	while (bytes_read <= BUFFER_SIZE && !ft_strfind(buffer, '\n') && read(fd, break_line, 1) > 0)
+	while (bytes_read <= BUFFER_SIZE && !ft_strfind(buffer, '\n')
+		&& read(fd, break_line, 1) > 0)
 	{
 		buffer[bytes_read] = break_line[0];
 		bytes_read++;
